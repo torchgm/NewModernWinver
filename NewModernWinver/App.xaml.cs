5a209b31ded3ebdc016cf92a2640a37c4354fc85
YourOrdinaryCat <49221554+YourOrdinaryCat@users.noreply.github.com>
2022-06-03 19:06:22 -0500

    Improved app initialization and activation code

--- NewModernWinver/App.xaml.cs
+++ NewModernWinver/App.xaml.cs
@@ -38,33 +38,10 @@ namespace NewModernWinver
         {
             if (args.Kind == ActivationKind.Protocol)
             {
-                ProtocolActivatedEventArgs eventArgs = args as ProtocolActivatedEventArgs;
+                ProtocolActivatedEventArgs e = args as ProtocolActivatedEventArgs;
 
-                // TODO: Handle URI activation
-                // The received URI is eventArgs.Uri.AbsoluteUri
-
-                Frame rootFrame = Window.Current.Content as Frame;
-
-                // Do not repeat app initialization when the Window already has content,
-                // just ensure that the window is active
-                if (rootFrame == null)
-                {
-                    // Create a Frame to act as the navigation context and navigate to the first page
-                    rootFrame = new Frame();
-
-                    rootFrame.NavigationFailed += OnNavigationFailed;
-
-
-                    // Place the frame in the current Window
-                    Window.Current.Content = rootFrame;
-                }
-
-                // Always navigate for a protocol launch
-                rootFrame.Navigate(typeof(MainPage));
-
-
-                // Ensure the current window is active
-                Window.Current.Activate();
+                Frame rootFrame = InitializeView();
+                rootFrame.Navigate(typeof(MainPage), e.Uri.AbsoluteUri);
             }
         }
 
@@ -75,37 +52,8 @@ namespace NewModernWinver
         /// <param name="e">Details about the launch request and process.</param>
         protected override void OnLaunched(LaunchActivatedEventArgs e)
         {
-            Frame rootFrame = Window.Current.Content as Frame;
-            // Do not repeat app initialization when the Window already has content,
-            // just ensure that the window is active
-            if (rootFrame == null)
-            {
-                // Create a Frame to act as the navigation context and navigate to the first page
-                rootFrame = new Frame();
-
-                rootFrame.NavigationFailed += OnNavigationFailed;
-
-                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
-                {
-                    //TODO: Load state from previously suspended application
-                }
-
-                // Place the frame in the current Window
-                Window.Current.Content = rootFrame;
-            }
-
-            if (e.PrelaunchActivated == false)
-            {
-                if (rootFrame.Content == null)
-                {
-                    // When the navigation stack isn't restored navigate to the first page,
-                    // configuring the new page by passing required information as a navigation
-                    // parameter
-                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
-                }
-                // Ensure the current window is active
-                Window.Current.Activate();
-            }
+            Frame rootFrame = InitializeView();
+            rootFrame.Navigate(typeof(MainPage), e.Arguments);
         }
 
         /// <summary>
