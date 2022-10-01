74a8dda882ffa09dc24a66989c6d4cd81b9bf7c2
JC Chuan <99233005+PoisonDarterz@users.noreply.github.com>
2022-10-01 22:08:09 +0800

    Add 22623 as 22H2 build

--- NewModernWinver/ListsAndStuff.cs
+++ NewModernWinver/ListsAndStuff.cs
@@ -26,7 +26,9 @@ namespace NewModernWinver
             { 20348, "21H2" },
             { 21996, "Stop using leaked builds" },
             { 22000, "21H2" },
-            { 22621, "22H2" }
+            { 22621, "22H2" },
+            { 22622, "22H2" },
+            { 22623, "22H2" }
         };
 
         public static Dictionary<int, string> EditionDict = new Dictionary<int, string>
